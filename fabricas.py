c07568e980ecf40296173fe8b7a28c29274584ef
MarlonStingelin <151676126+MarlonStingelin@users.noreply.github.com>
2025-07-05 18:51:42 -0300

    Create fabricas.py

---
+++ fabricas.py
@@ -0,0 +1,16 @@
+# from abstracoes import IFabricaEstilos, IEncaixe # (Em arquivo separado)
+# from componentes import Entrada, Saida # (Em arquivo separado)
+
+class FabricaEstiloConcreta(IFabricaEstilos):
+    def __init__(self, estilo_id: int):
+        if estilo_id <= 0:
+            raise ValueError("ID do Estilo deve ser um inteiro positivo.")
+        self._estilo_id = estilo_id
+
+    def criar_entrada(self) -> IEncaixe:
+        tipo_id = (2 * self._estilo_id) - 1
+        return Entrada(tipo_id)
+
+    def criar_saida(self) -> IEncaixe:
+        tipo_id = 2 * self._estilo_id
+        return Saida(tipo_id)
