4e82b7fed4eec07d22f142330ad68b8e67b6724f
MarlonStingelin <151676126+MarlonStingelin@users.noreply.github.com>
2025-07-05 18:52:41 -0300

    Create gerenciador.py

---
+++ gerenciador.py
@@ -0,0 +1,17 @@
+from typing import Dict
+# from abstracoes import IPecaPrototipo # (Em arquivo separado)
+
+class GerenciadorDePrototipos:
+    """cópias de peças"""
+    def __init__(self):
+        self._prototipos: Dict[str, IPecaPrototipo] = {}
+
+    def adicionar_prototipo(self, chave: str, peca: IPecaPrototipo):
+        self._prototipos[chave] = peca
+
+    def obter_peca_clonada(self, chave: str) -> IPecaPrototipo:
+        prototipo = self._prototipos.get(chave)
+        if not prototipo:
+            raise ValueError(f"Protótipo com a chave '{chave}' não encontrado.")
+        return prototipo.clonar()
+      
