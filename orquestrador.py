5f8dda5afbe10b9190a7bb5d62e7dc7fb0b6328d
MarlonStingelin <151676126+MarlonStingelin@users.noreply.github.com>
2025-07-05 19:06:37 -0300

    Update orquestrador.py

--- orquestrador.py
+++ orquestrador.py
@@ -20,10 +20,8 @@ class GeradorQuebraCabeca:
         self._gerenciador_prototipos.adicionar_prototipo("meio", Peca())
         
         pecas_com_bordas = {
-            "borda_norte": {"norte": BordaReta()},
-            "borda_sul": {"sul": BordaReta()},
-            "borda_leste": {"leste": BordaReta()},
-            "borda_oeste": {"oeste": BordaReta()},
+            "borda_norte": {"norte": BordaReta()}, "borda_sul": {"sul": BordaReta()},
+            "borda_leste": {"leste": BordaReta()}, "borda_oeste": {"oeste": BordaReta()},
             "canto_no": {"norte": BordaReta(), "oeste": BordaReta()},
             "canto_ne": {"norte": BordaReta(), "leste": BordaReta()},
             "canto_so": {"sul": BordaReta(), "oeste": BordaReta()},
@@ -51,9 +49,26 @@ class GeradorQuebraCabeca:
         if e_borda_leste: return "borda_leste"
         return "meio"
 
+    def _criar_deck_de_fabricas_balanceado(self) -> List[IFabricaEstilos]:
+        conexoes_horizontais = self._altura * (self._comprimento - 1)
+        conexoes_verticais = self._comprimento * (self._altura - 1)
+        total_conexoes = conexoes_horizontais + conexoes_verticais
+        
+        num_estilos = len(self._fabricas_de_estilos)
+        deck = []
+        
+        for i in range(total_conexoes):
+            fabrica = self._fabricas_de_estilos[i % num_estilos]
+            deck.append(fabrica)
+            
+        random.shuffle(deck) 
+        return deck
+
     def gerar_grade_de_pecas(self) -> List[str]:
         if not self._fabricas_de_estilos:
             raise ValueError("Nenhum estilo de encaixe foi definido.")
+
+        deck_de_fabricas = self._criar_deck_de_fabricas_balanceado()
 
         grade_de_pecas = [[None for _ in range(self._comprimento)] for _ in range(self._altura)]
 
@@ -64,38 +79,36 @@ class GeradorQuebraCabeca:
                 peca_atual.set_posicao(y, x)
                 grade_de_pecas[y][x] = peca_atual
 
-                # Norte
                 if y > 0:
                     vizinho_norte = grade_de_pecas[y-1][x]
                     encaixe_sul_vizinho = vizinho_norte.get_encaixe("sul")
-                    fabrica_usada = self._fabricas_de_estilos[ (encaixe_sul_vizinho.get_tipo_id() // 2) -1 ]
+                    id_fabrica_usada = (encaixe_sul_vizinho.get_tipo_id() -1) // 2
+                    fabrica_usada = self._fabricas_de_estilos[id_fabrica_usada]
                     
                     if isinstance(encaixe_sul_vizinho, Entrada):
                         peca_atual.set_encaixe("norte", fabrica_usada.criar_saida())
                     else:
                         peca_atual.set_encaixe("norte", fabrica_usada.criar_entrada())
                 
-                # Oeste
                 if x > 0:
                     vizinho_oeste = grade_de_pecas[y][x-1]
                     encaixe_leste_vizinho = vizinho_oeste.get_encaixe("leste")
-                    fabrica_usada = self._fabricas_de_estilos[ (encaixe_leste_vizinho.get_tipo_id() // 2) -1 ]
+                    id_fabrica_usada = (encaixe_leste_vizinho.get_tipo_id() -1) // 2
+                    fabrica_usada = self._fabricas_de_estilos[id_fabrica_usada]
 
                     if isinstance(encaixe_leste_vizinho, Entrada):
                         peca_atual.set_encaixe("oeste", fabrica_usada.criar_saida())
                     else:
                         peca_atual.set_encaixe("oeste", fabrica_usada.criar_entrada())
 
-                # Abaixo Leste e Sul
                 for direcao in ["sul", "leste"]:
                     if peca_atual.get_encaixe(direcao) is None:
-                        fabrica = random.choice(self._fabricas_de_estilos)
+                        # reinicia o baralho
+                        if not deck_de_fabricas:
+                            deck_de_fabricas = self._criar_deck_de_fabricas_balanceado()
+                        
+                        fabrica = deck_de_fabricas.pop()
                         peca_atual.set_encaixe(direcao, random.choice([fabrica.criar_entrada(), fabrica.criar_saida()]))
 
-        # Saída
-        saidas_formatadas = []
-        for y in range(self._altura):
-            for x in range(self._comprimento):
-                saidas_formatadas.append(grade_de_pecas[y][x].gerar_saida_formatada())
-        
+        saidas_formatadas = [grade_de_pecas[y][x].gerar_saida_formatada() for y in range(self._altura) for x in range(self._comprimento)]
         return saidas_formatadas
